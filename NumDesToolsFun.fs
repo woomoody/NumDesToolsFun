761a929bcab1265f7e318608c1c592a3e7f0042a
cent <cent@newborntown.com>
2025-12-27 15:12:31 +0800

    添加项目文件。

---
+++ NumDesToolsFun.fs
@@ -0,0 +1,7 @@
+﻿namespace NumDesToolsFun
+
+module NumDesToolsFun = 
+    open ExcelDna.Integration
+
+    [<ExcelFunction(Description = "My first .NET function")>]
+    let SayHello name = "Hello " + name
