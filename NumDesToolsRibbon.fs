b48791ebed910e2906f4d66cf4eb7e699fcaac75
cent <cent@newborntown.com>
2025-12-30 14:44:59 +0800

    Ribbon提取为文件

--- NumDesToolsRibbon.fs
+++ NumDesToolsRibbon.fs
@@ -5,6 +5,9 @@ open System.Runtime.InteropServices
 open Microsoft.Office.Interop.Excel
 open ExcelDna.Integration
 open ExcelDna.Integration.CustomUI
+open System.Reflection
+open System.IO   
+open System.Text
 
 // This defines a regular Excel macro (in Excel you can press Alt + F8, type in the name "showMessage", then click the Run button).
 // For the ribbon, it will be run through the ExcelRibbon.RunTagMacro(...) helper, which run whatever macro is specified in the button tag attribute
@@ -22,20 +25,44 @@ type public MyRibbon() =
 
     // The ribbon xml definition could also be placed in the .dna file
     // Remember to switch on the ExcelOption "Show add-in user interface errors" option (under the Advanced tab under General)
-    override this.GetCustomUI(ribbonId) = 
-        @"<customUI xmlns='http://schemas.microsoft.com/office/2006/01/customui' >
-          <ribbon>
-            <tabs>
-              <tab id='CustomTab' label='My F# Tab'>
-                <group id='SampleGroup' label='My Sample Group'>
-                  <button id='Button1' label='Run a macro' onAction='RunTagMacro' tag='showMessage' />
-                  <button id='Button2' label='Run a class member' onAction='OnButtonPressed'/>
-                  <button id='Button3' label='Dump the Excel Version to cell A1' onAction='OnDumpData'/>
-                </group >
-              </tab>
-            </tabs>
-          </ribbon>
-        </customUI>"
+
+    override this.GetCustomUI(ribbonId) =
+        let mutable ribbonXml = ""
+        try
+            // 调用资源读取函数
+            ribbonXml <- this.GetRibbonXml("Ribbon.xml")
+
+            // 调试模式下的字符串替换
+#if DEBUG
+            ribbonXml <- ribbonXml.Replace(
+                "<tab id='MainTab' label='NumDesToolsF#' insertBeforeMso='TabHome'>",
+                "<tab id='MainTab' label='N*D*T*DebugF#' insertBeforeMso='TabHome'>"
+            )
+            ribbonXml <- ribbonXml.Replace(
+                "<tab id='SecondTab' label='NumDesToolsF#Plus' insertBeforeMso='TabHome'>",
+                "<tab id='SecondTab' label='N*D*T*PlusDebugF#' insertBeforeMso='TabHome'>"
+            )
+#endif
+        with
+        | ex -> MessageBox.Show(ex.Message) |> ignore // F# 中忽略返回值使用 |>
+
+        ribbonXml
+
+    // 内部方法，用于从嵌入式资源读取 Ribbon XML
+    member private this.GetRibbonXml(resourceName: string) =
+        let assn = Assembly.GetExecutingAssembly()
+        let resources = assn.GetManifestResourceNames()
+        let mutable text = ""
+
+        for resource in resources do
+            if resource.EndsWith(resourceName) then
+                use streamText = assn.GetManifestResourceStream(resource)
+                if streamText <> null then
+                    use reader = new StreamReader(streamText)
+                    text <- reader.ReadToEnd()
+                // F# 的 `use` 关键字会自动处理资源释放，无需手动 Close()
+
+        text
 
     member this.OnButtonPressed (control:IRibbonControl) =
         MessageBox.Show "Hello from F#!" 
